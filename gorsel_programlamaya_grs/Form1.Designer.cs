4db7687129fb9ced21ab615a3d657a14cfc4124a
yusufcannn <yusufcauans@gmail.com>
2022-03-27 23:11:06 +0300

    first comment

---
+++ gorsel_programlamaya_grs/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿
+namespace gorsel_programlamaya_grs
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 53);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1124, 463);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1148, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(995, 30);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(58, 17);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "square";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(665, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(787, 24);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(82, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Color";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(908, 30);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(51, 17);
+            this.checkBox2.TabIndex = 7;
+            this.checkBox2.Text = "circle";
+            this.checkBox2.UseVisualStyleBackColor = true;
+          
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1148, 537);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox checkBox2;
+    }
+}
+
---
+++ gorsel_programlamaya_grs/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing; // windows cizim kutuphanesi
+using System.Drawing.Drawing2D; // windows 2d cizim kutuphanesi
+using System.Drawing.Imaging; // windows resim kutuphanesi
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gorsel_programlamaya_grs
+{
+    public partial class Form1 : Form
+    {
+
+       
+        Point lastLocation = Point.Empty; // Bos Nokta sınıfı olustruluyor yani son kordinat alınması için
+        Pen pen = new Pen(Color.Black); // varsayılan olarak kalem rengi siyah olarak ayarladım
+        bool isMouseDown = new Boolean(); //  bir bool instance aldım fareye tıklanıp tıklanmadıgını anlamak için
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e) // picturebox mousemove event handler olusturdum
+        {
+            if (isMouseDown == true) // eger fareye  tıklanıyorsa
+            {
+                if (lastLocation != null)  // eger son kordinat null değilse 
+                {
+                    if (checkBox1.Checked ==false && checkBox2.Checked == false) // eger 2 checkboxa checklenmediyse
+                    {
+                        if (pictureBox1.Image == null) // picturebox image properties null ise
+                        {
+                            Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height); // bitmap sınıfı olusturup picturebox yukseklik ve genislik degeri bmp instance edilir
+                            pictureBox1.Image = bmp; // picturebox image propertiese bmp aktarılır
+                        }
+                        using (Graphics g = Graphics.FromImage(pictureBox1.Image)) // graphic classından fromImage prop picturbox baz alınarak olsuturulur
+                        {
+
+                            var point1 = new Point(234, 118); // tamamen rasgele bir kordinat
+                            var point2 = new Point(293, 228); // tamamen rasgele bir kordinat
+                            g.DrawLine(pen, lastLocation, e.Location); // graphic DrawLine prob kalem son kordinat ve mevcut mouse kordinatını eş zamanlı olarak verdim
+                        }
+                        pictureBox1.Invalidate(); // picturebox yeniden çizilmesini sağlar
+                        lastLocation = e.Location; // mouse ile aldıgımız location bilgisini son koordinata aktarılır
+                    }
+                   
+                }
+                
+            }
+           
+           
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e) // picturebox mousdown event handler olusturdum
+        {
+            lastLocation = e.Location; // mouse ile aldıgımız location bilgisini son koordinata aktarılır
+            isMouseDown = true; // fareye tiklandı
+            if (checkBox1.Checked == true && checkBox2.Checked !=true) // checkbox check olursa ve check2 check olmasa
+            {
+                if (pictureBox1.Image == null) // picturebox image properties null ise
+                {
+                    Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height); // bitmap sınıfı olusturup picturebox yukseklik ve genislik degeri bmp instance edilir
+                    pictureBox1.Image = bmp;// picturebox image propertiese bmp aktarılır
+                }
+                using (Graphics g = Graphics.FromImage(pictureBox1.Image)) // graphic classından fromImage prop picturbox baz alınarak olsuturulur
+                {
+                    Rectangle rect = new Rectangle(lastLocation, (Size)e.Location); // bir tane dikdortgen instane aldık paramtre olarak mevcut koordinati verdik ve son koordinatı verdim
+                    g.DrawRectangle(pen, rect); // dikdortgen cizdirdik
+                }
+                pictureBox1.Invalidate(); // picturebox yeniden çizilmesini sağlar
+                lastLocation = e.Location; // mouse ile aldıgımız location bilgisini son koordinata aktarılır
+            }
+            if (checkBox2.Checked == true && checkBox1.Checked !=true)
+            {
+                if (pictureBox1.Image == null) // picturebox image properties null ise
+                {
+                    Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height); // bitmap sınıfı olusturup picturebox yukseklik ve genislik degeri bmp instance edilir
+                    pictureBox1.Image = bmp;// picturebox image propertiese bmp aktarılır
+                }
+                using (Graphics g = Graphics.FromImage(pictureBox1.Image)) // graphic classından fromImage prop picturbox baz alınarak olsuturulur
+                {
+                    Rectangle rect = new Rectangle(lastLocation, (Size)e.Location); // bir tane dikdortgen instane aldık paramtre olarak mevcut koordinati verdik ve son koordinatı verdim
+                    g.DrawEllipse(pen,rect); // cember cizdirdik
+                }
+                pictureBox1.Invalidate(); // picturebox yeniden çizilmesini sağlar
+                lastLocation = e.Location; // mouse ile aldıgımız location bilgisini son koordinata aktarılır
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)// button click   event handler olusturdum
+        {
+            if (pictureBox1.Image != null)// picturebox image properties null degil ise
+
+            {
+
+                pictureBox1.Image = null;// picturebox image properties null ise
+
+                pictureBox1.Invalidate(); // picturebox yeniden çizilmesini sağlar
+
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e) // picturebox mouseup event handler olusturdum
+        {
+            
+            isMouseDown = false; // mouse tiklanmadı
+
+            lastLocation = Point.Empty; // son kordinatı bosaltık
+        }
+
+        private void button2_Click(object sender, EventArgs e) // button click 2 event handler olsuturuldu
+        {
+           
+            if(colorDialog1.ShowDialog() == DialogResult.OK) // colordialog.show onaylandı ise pen colora colordialog color ata
+            {
+                pen.Color = colorDialog1.Color;
+            }
+
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e) // menustrip open click event handler olsuturuluyor
+        {
+            // open file dialog   
+            OpenFileDialog open = new OpenFileDialog(); // open file dialog instance
+            // image filters  
+            open.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp; *.png)|*.jpg; *.jpeg; *.gif; *.bmp; *.png"; // image uzantılarını filtrelemk için
+            if (open.ShowDialog() == DialogResult.OK)// open.showDialgo onaylandı ise pictureboxa yeni seçilan dosyayı bitmap tipinde aktar
+            {
+                // display image in picture box  
+                pictureBox1.Image = new Bitmap(open.FileName);
+               
+               
+            }
+        }
+
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)  // menustrip save click event handler olsuturuluyor
+        {
+            SaveFileDialog save = new SaveFileDialog();// save file dialog instance
+            save.Filter = "Images|*.png;*.bmp;*.jpg";// image uzantılarını filtrelemk için
+            ImageFormat format = ImageFormat.Png; // image png formatında kontrol için
+            if (save.ShowDialog() == DialogResult.OK) // save.showDialgo onaylandı ise 
+            {
+                string ext = Path.GetExtension(save.FileName); // dosyanın uzantısı alınır
+                switch (ext) // ext kontrol edilir
+                {
+                    case ".jpg": // jpg ise formatı picturebox gore ayarla
+                        format = ImageFormat.Jpeg;
+                        break;
+                    case ".bmp":// bmp ise formatı picturebox gore ayarla
+                        format = ImageFormat.Bmp;
+                        break;
+                    case ".png": // png ise formatı picturebox gore ayarla
+                        format = ImageFormat.Png;
+                        break;
+                }
+                pictureBox1.Image.Save(save.FileName, format); // picturebox dosyayı kaydet
+            }
+        }
+
+    }
+}
